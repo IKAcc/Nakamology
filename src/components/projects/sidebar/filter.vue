b53660a67cb5fee6ac9a4f13bb3d8f611f6ca64d
Javid Izadfar <j.izadfar@medgo.co>
2021-06-08 17:00:59 +0430

    made filters scroll + added scroll style

--- src/components/projects/sidebar/filter.vue
+++ src/components/projects/sidebar/filter.vue
@@ -1,24 +1,26 @@
 <template>
     <div class="p-2">
-        <div class="card">
+        <div class="card overflow-hidden">
             <span class="block text-2xl text-white font-bold mb-4 font-serif">دسته‌ها</span>
-            <ul>
-                <li v-for="tag in tags"
-                    :key="tag.id">
-                    <g-link :to="tag.path" class="item flex items-center -my-2 cursor-pointer">
-                        <span class="radio">
-                            <span class="radio__bilbil"/>
-                        </span>
-                        <span class="p-2">
-                            {{ tag.title }}
-                        </span>
-                        <span v-if="tag.totalCount"
-                              class="chips min-w-6">
-                            {{ tag.totalCount | toFarsiDigits }}
-                        </span>
-                    </g-link>
-                </li>
-            </ul>
+            <div class="relative">
+                <ul class="max-h-72 overflow-y-auto -m-4 p-4">
+                    <li v-for="tag in tags"
+                        :key="tag.id">
+                        <g-link :to="tag.path" class="item flex items-center -my-2 cursor-pointer">
+                            <span class="radio">
+                                <span class="radio__bilbil"/>
+                            </span>
+                            <span class="p-2">
+                                {{ tag.title }}
+                            </span>
+                            <span v-if="tag.totalCount"
+                                  class="chips min-w-6">
+                                {{ tag.totalCount | toFarsiDigits }}
+                            </span>
+                        </g-link>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -80,6 +82,18 @@ export default {
 }
 .radio__bilbil {
     @apply block w-full h-full rounded-full
+}
+ul::after,
+ul::before {
+    content: '';
+    @apply absolute right-0 w-full h-5 from-nk-gray-300;
+}
+
+ul::after {
+    @apply top-0 bg-gradient-to-b;
+}
+ul::before {
+    @apply bottom-0 bg-gradient-to-t;
 }
 
 .item:hover,
